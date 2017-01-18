ce36a1a29cb60ace3769f1ad11e99dba41a11b6c
Vladimir Kiryakov <vovakiryakov@gmail.com>
2017-01-18 16:30:20 +0600

    stack

--- five/stack-layout/app.js
+++ five/stack-layout/app.js
@@ -4,83 +4,58 @@ document.addEventListener("DOMContentLoaded", function () {
   init();
 
   function init() {
-    d3.json("tweets.json", viz)
+    d3.csv("movies.csv", viz)
   }
 
 
   function viz(data) {
 
-    var depthScale = d3.scaleOrdinal()
-      .range(["#5EAFC6", "#FE9922", "#93c464", "#75739F"]);
 
-    var nestedTweets = d3.nest()
-      .key(function (d) {
-        return d.user
-      })
-      .entries(data.tweets);
-    var packableTweets = {id: "All Tweets", values: nestedTweets};
+    var xScale = d3.scaleLinear().domain([0, 10]).range([0, 500])
+    var yScale = d3.scaleLinear().domain([0, 60]).range([480, 0])
+    var heightScale = d3.scaleLinear().domain([0, 60]).range([0, 480])
+    var movies = ["titanic", "avatar", "akira", "frozen", "deliverance", "avengers"]
 
-    var root = d3.hierarchy(packableTweets, function (d) {
-      return d.values
-    });
+    var fillScale = d3.scaleOrdinal()
+      .domain(movies)
+      .range(["#fcd88a", "#cf7c1c", "#93c464", "#75734F", "#5eafc6", "#41a368"])
 
-    var treeChart = d3.tree();
-    treeChart.size([200, 200]);
+    var xAxis = d3.axisBottom()
+      .scale(xScale)
+      .tickSize(500)
+      .tickValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
-    var treeData = treeChart(root).descendants();
+    d3.select("svg").append("g").attr("id", "xAxisG").call(xAxis);
+    var yAxis = d3.axisRight()
+      .scale(yScale)
+      .ticks(10)
+      .tickSize(530);
+    d3.select("svg").append("g").attr("id", "yAxisG").call(yAxis);
 
-    d3.select("svg")
-      .append("g")
-      .attr("id", "treeG")
-      .attr("transform", "translate(250,250)")
-      .selectAll("g")
-      .data(treeData)
+    stackLayout = d3.stack().keys(movies)
+
+    d3.select("svg").selectAll("g.bar")
+      .data(stackLayout(data))
       .enter()
       .append("g")
-      .attr("class", "node")
-      .attr("transform", function (d) {
-        return "translate(" + project(d.x , d.y) + ")"
+      .attr("class", "bar")
+      .each(function (d) {
+        d3.select(this).selectAll("rect")
+          .data(d)
+          .enter()
+          .append("rect")
+          .attr("x", function (p, q) {
+            return xScale(q) + 30
+          })
+          .attr("y", function (p) {
+            return yScale(p[1])
+          })
+          .attr("height", function (p) {
+            return heightScale(p[1] - p[0])
+          })
+          .attr("width", 40)
+          .style("fill", fillScale(d.key));
       });
-
-    d3.selectAll("g.node")
-      .append("circle")
-      .attr("r", 10)
-      .style("fill", function (d) {
-        return depthScale(d.depth)
-      })
-      .style("stroke", "white")
-      .style("stroke-width", "2px");
-
-    d3.selectAll("g.node")
-      .append("text")
-      .style("text-anchor", "middle")
-      .style("fill", "#4f442b")
-      .text(function(d){ return d.data.id || d.data.key /*|| d.data.content*/});
-
-    d3.select("#treeG").selectAll("line")
-      .data(treeData.filter(function (d) {
-        return d.parent
-      }))
-      .enter().insert("line", "g")
-      .attr("x1", function (d) {
-        return project(d.parent.x, d.parent.y)[0]
-      })
-      .attr("y1", function (d) {
-        return project(d.parent.x, d.parent.y)[1]
-      })
-      .attr("x2", function (d) {
-        return project(d.x, d.y)[0]
-      })
-      .attr("y2", function (d) {
-        return project(d.x, d.y)[1]
-      })
-      .style("stroke", "black")
-
-    function project(x, y) {
-      var angle = x / 90 * Math.PI;
-      var radius = y;
-      return [radius * Math.cos(angle), radius * Math.sin(angle)];
-    }
 
 
   }
