255f626d1091e12bc922c436c3fe5d23115a129f
Vladimir Kiryakov <vovakiryakovl@gmail.com>
2017-01-19 16:21:43 +0600

    adjacency matrix

--- six/app.js
+++ six/app.js
@@ -4,39 +4,47 @@ document.addEventListener("DOMContentLoaded", function () {
   init();
 
   function init() {
-    d3.json("tweets.json", viz)
-  }
-
-
-  function adjacency() {
-
-    queue()
-      .defer(d3.csv, "nodelist.csv")
-      .defer(d3.csv, "edgelist.csv")
-      .await(function (error, file1, file2) {
-        createAdjacencyMatrix(file1, file2);
+    var loadCsv = function (d) {
+      return new Promise(function (resolve) {
+        return d3.csv(d, function (p) {
+          return resolve(p)
+        })
       });
-    function createAdjacencyMatrix(nodes, edges) {
-      var edgeHash = {};
-      for (x in edges) {
-        var id = edges[x].source + "-" + edges[x].target;
-        edgeHash[id] = edges[x];
-      }
+    };
 
-      matrix = [];
-      for (a in nodes) {
-        for (b in nodes) {
-          var grid =
-          {
-            id: nodes[a].id + "-" + nodes[b].id,
-            x: b, y: a, weight: 0
+    Promise
+      .all([
+        loadCsv("nodelist.csv"),
+        loadCsv("edgelist.csv")
+      ])
+      .then(function (resolve) {
+        return createAdjacencyMatrix(resolve[0], resolve[1])
+      });
+
+
+    function createAdjacencyMatrix(nodes, edges) {
+
+      var edgeHash = {};
+      edges.forEach(function (edge) {
+        var id = edge.source + "-" + edge.target;
+        edgeHash[id] = edge
+      });
+
+      var matrix = [];
+      nodes.forEach(function (source, a) {
+        nodes.forEach(function (target, b) {
+          var grid = {
+            id: source.id + "-" + target.id,
+            x: b,
+            y: a,
+            weight: 0
           };
           if (edgeHash[grid.id]) {
             grid.weight = edgeHash[grid.id].weight;
           }
           matrix.push(grid);
-        }
-      }
+        })
+      });
 
       d3.select("svg")
         .append("g")
@@ -49,29 +57,48 @@ document.addEventListener("DOMContentLoaded", function () {
         .attr("class", "grid")
         .attr("width", 25)
         .attr("height", 25)
-        .attr("x", function (d) {return d.x * 25})
-        .attr("y", function (d) {return d.y * 25})
-        .style("fill-opacity", function (d) {return d.weight * .2;})
+        .attr("x", function (d) {
+          return d.x * 25
+        })
+        .attr("y", function (d) {
+          return d.y * 25
+        })
+        .style("fill-opacity", function (d) {
+          return d.weight * .2;
+        });
 
-
-      var scaleSize = nodes.length * 25;
-      var nameScale = d3.scale.ordinal()
-        .domain(nodes.map(function (el) {return el.id}))
-        .rangePoints([0,scaleSize],1);
-      var xAxis = d3.svg.axis()
-        .scale(nameScale).orient("top").tickSize(4);
-      var yAxis = d3.svg.axis()
-        .scale(nameScale).orient("left").tickSize(4);
-      d3.select("#adjacencyG").append("g").call(yAxis);
-      d3.select("#adjacencyG").append("g").call(xAxis)
+      d3.select("svg")
+        .append("g")
+        .attr("transform", "translate(50,45)")
         .selectAll("text")
+        .data(nodes)
+        .enter()
+        .append("text")
+        .attr("x", function (d, i) {
+          return i * 25 + 12.5
+        })
+        .text(function (d) {
+          return d.id
+        })
+        .style("text-anchor", "middle")
+
+      d3.select("svg")
+        .append("g")
+        .attr("transform", "translate(45,50)")
+        .selectAll("text")
+        .data(nodes)
+        .enter()
+        .append("text")
+        .attr("y", function (d, i) {
+          return i * 25 + 12.5
+        })
+        .text(function (d) {
+          return d.id
+        })
         .style("text-anchor", "end")
-        .attr("transform", "translate(-10,-10) rotate(90)");
-
     }
-
-
   }
+
 
 });
 
