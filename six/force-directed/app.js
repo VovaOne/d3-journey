50e3c4e372e994278f347dbeb60f8ef3cad44e39
Vladimir Kiryakov <vovakiryakov@gmail.com>
2017-01-23 13:33:13 +0600

    force layout

--- six/force-directed/app.js
+++ six/force-directed/app.js
@@ -4,98 +4,188 @@ document.addEventListener("DOMContentLoaded", function () {
   init();
 
   function init() {
-    var loadCsv = function (d) {
-      return new Promise(function (resolve) {
-        return d3.csv(d, function (p) {
-          return resolve(p)
-        })
-      });
-    };
-
     Promise
       .all([
         loadCsv("nodelist.csv"),
         loadCsv("edgelist.csv")
       ])
       .then(function (resolve) {
-        return createAdjacencyMatrix(resolve[0], resolve[1])
+        return visualize(resolve[0], resolve[1])
       });
 
   }
 
 
-  function createAdjacencyMatrix(nodes, edges) {
+  function visualize(nodes, edges) {
+
+
+    var roleScale = d3.scaleOrdinal()
+      .domain(["contractor", "employee", "manager"])
+      .range(["#75739F", "#41A368", "#FE9922"]);
+
+    var marker = d3.select("svg")
+      .append('defs')
+      .append('marker')
+      .attr("id", "Triangle")
+      .attr("refX", 12)
+      .attr("refY", 6)
+      .attr("markerUnits", 'userSpaceOnUse')
+      .attr("markerWidth", 12)
+      .attr("markerHeight", 18)
+      .attr("orient", 'auto')
+      .append('path')
+      .attr("d", 'M 0 0 12 6 0 12 3 6');
 
     var nodeHash = {};
-    for (x in nodes) {
-      nodeHash[nodes[x].id] = nodes[x];
-      nodes[x].x = parseInt(x) * 40;
-    }
+    nodes.forEach(function (node) {
+      return nodeHash[node.id] = node;
+    });
 
-    for (x in edges) {
-      edges[x].weight = parseInt(edges[x].weight);
-      edges[x].source = nodeHash[edges[x].source];
-      edges[x].target = nodeHash[edges[x].target];
-    }
+    edges.forEach(function (edge) {
+      edge.weight = parseInt(edge.weight);
+      edge.source = nodeHash[edge.source];
+      edge.target = nodeHash[edge.target];
+    });
 
-    var linkScale = d3.scaleLinear()
-      .domain(d3.extent(edges, function (d) {
-        return d.weight
-      }))
-      .range([5, 10]);
+    nodes.forEach(function (d) {
+      d.degreeCentrality = edges.filter(function (p) {
+        return p.source === d || p.target === d
+      }).length
+    });
 
-    var arcG = d3.select("svg").append("g").attr("id", "arcG")
-      .attr("transform", "translate(50,250)");
+    console.log(nodes);
 
-    arcG.selectAll("path")
-      .data(edges)
-      .enter()
-      .append("path")
-      .attr("class", "arc")
-      .style("stroke-width", function (d) {
-        return d.weight * 2;
+    var linkForce = d3.forceLink().strength(function (d) {
+      return d.weight * .1
+    });
+
+    var simulation = d3.forceSimulation()
+      .force("charge", d3.forceManyBody().strength(-500))
+      .force("x", d3.forceX(250))
+      .force("y", d3.forceY(250))
+      .force("link", linkForce)
+      .nodes(nodes)
+      .on("tick", forceTick);
+
+    simulation.force("link").links(edges);
+
+    d3.select("svg").selectAll("line.link")
+      .data(edges, function (d) {
+        return d.source.id + "-" + d.target.id
       })
-      .style("opacity", .25)
-      .attr("d", arc);
-
-    arcG.selectAll("circle")
-      .data(nodes)
       .enter()
+      .append("line")
+      .attr("class", "link")
+      .style("opacity", .5)
+      .style("stroke-width", function (d) {
+        return d.weight
+      });
+
+    d3.selectAll("line").attr("marker-end", "url(#Triangle)");
+
+    var nodeEnter = d3.select("svg").selectAll("g.node")
+      .data(nodes, function (d) {
+        return d.id
+      })
+      .enter()
+      .append("g")
+      .attr("class", "node");
+
+    nodeEnter
       .append("circle")
-      .attr("class", "node")
-      .attr("r", 10)
-      .attr("cx", function (d) {
-        return d.x;
+      .attr("r", function (d) {
+        return d.degreeCentrality * 2;
+      })
+      .style("fill", function (d) {
+        return roleScale(d.role)
       });
 
-    function arc(d, i) {
-      var draw = d3.line().curve(d3.curveBasis);
-      var midX = (d.source.x + d.target.x) / 2;
-      var midY = (d.source.x - d.target.x) * 2;
-      return draw([[d.source.x, 0], [midX, midY], [d.target.x, 0]])
+    nodeEnter.append("text")
+      .style("text-anchor", "middle")
+      .attr("y", 15)
+      .text(function (d) {
+          return d.id
+        }
+      );
+
+    d3.select("svg").on("click", manuallyPositionNodes);
+
+    function manuallyPositionNodes() {
+      var xExtent = d3.extent(simulation.nodes(), function (d) {
+        return parseInt(d.degreeCentrality)
+      });
+      var yExtent = d3.extent(simulation.nodes(), function (d) {
+        return parseInt(d.salary)
+      });
+      var xScale = d3.scaleLinear().domain(xExtent).range([50, 450]);
+      var yScale = d3.scaleLinear().domain(yExtent).range([450, 50]);
+      simulation.stop();
+      d3.selectAll("g.node")
+        .transition()
+        .duration(1000)
+        .attr("transform", function (d) {
+          return "translate(" + xScale(d.degreeCentrality)
+            + "," + yScale(d.salary) + ")"
+        });
+      d3.selectAll("line.link")
+        .transition()
+        .duration(1000)
+        .attr("x1", function (d) {
+          return xScale(d.source.degreeCentrality)
+        })
+        .attr("y1", function (d) {
+          return yScale(d.source.salary)
+        })
+        .attr("x2", function (d) {
+          return xScale(d.target.degreeCentrality)
+        })
+        .attr("y2", function (d) {
+          return yScale(d.target.salary)
+        });
+      var xAxis = d3.axisBottom().scale(xScale).tickSize(4);
+      var yAxis = d3.axisRight().scale(yScale).tickSize(4);
+      d3.select("svg").append("g").attr("transform",
+        "translate(0,460)").call(xAxis);
+      d3.select("svg").append("g").attr("transform",
+        "translate(460,0)").call(yAxis);
+      d3.selectAll("g.node").each(function (d) {
+        d.x = xScale(d.degreeCentrality);
+        d.vx = 0;
+        d.y = yScale(d.salary);
+        d.vy = 0;
+      })
+
     }
 
-    d3.selectAll("circle").on("mouseover", nodeOver);
-    d3.selectAll("path").on("mouseover", edgeOver);
+  }
 
-    function nodeOver(d, i) {
-      d3.selectAll("circle").classed("active", function (p) {
-        return p == d ? true : false;
+  function forceTick() {
+    d3.selectAll("line.link")
+      .attr("x1", function (d) {
+        return d.source.x;
+      })
+      .attr("x2", function (d) {
+        return d.target.x;
+      })
+      .attr("y1", function (d) {
+        return d.source.y;
+      })
+      .attr("y2", function (d) {
+        return d.target.y;
       });
-      d3.selectAll("path").classed("active", function (p) {
-        return p.source == d || p.target == d ? true : false;
-      });
-    }
+    d3.selectAll("g.node")
+      .attr("transform", function (d) {
+        return "translate(" + d.x + "," + d.y + ")";
+      })
+  }
 
-    function edgeOver(d) {
-      d3.selectAll("path").classed("active", function (p) {
-        return p == d ? true : false;
-      });
-      d3.selectAll("circle").style("fill", function (p) {
-        return p == d.source ? "blue" : p == d.target ? "green" : "lightgray";
-      });
-    }
 
+  function loadCsv(d) {
+    return new Promise(function (resolve) {
+      return d3.csv(d, function (p) {
+        return resolve(p)
+      })
+    });
   }
 
 
